495fcdaf3a4d218050310269865c62ef9fc0a6f4
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-11-19 01:52:04 +0530

    done basics contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
   /**
    * Sticky header on scroll
    */
@@ -147,5 +156,23 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     aos_init();
   });
+
+
+let countdown = select('.countdown');
+     const output = countdown.innerHTML;
+   
+     const countDownDate = function() {
+       let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
+   
+       let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+       let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+       let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+       let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+   
+       countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+     }
+     countDownDate();
+     setInterval(countDownDate, 1000);
+  
 
 });
